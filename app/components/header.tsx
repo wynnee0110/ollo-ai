ca1fded0564b11a27648b4a3a31248ff59422705
wynnee0110 <wynnee0110@gmail.com>
2026-01-15 22:06:03 +0800

    hello

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,19 +2,27 @@
 export default function () {
 return   (
     <>
-
     <div className="w-full h-10 ">
-        <div className="flex justify-between text-white p-2">
-        <div className="px-2">
-            <h2>Ollo</h2>
+        <div className="flex justify-between p-2">
+            <div className="px-2" >
+                ollo
+
+
+
+
+            </div>
+            <div className="px-2">
+                login
+              
+
+
+
+            </div>
+
         </div>
-        <div className="px-2">
-            login
-       
-        </div>
+
+
     </div>
-    </div>
-    
     </>
 )
 
